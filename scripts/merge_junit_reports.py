c2eb78574365b86d365ff9fa81de25deaff215a6
vayaszsoltmarton <168715287+vayaszsoltmarton@users.noreply.github.com>
2024-09-13 22:29:04 +0200

    Update merge_junit_reports.py

--- scripts/merge_junit_reports.py
+++ scripts/merge_junit_reports.py
@@ -1,1 +1,17 @@
+import os
+from junitparser import JUnitXml
 
+# Directory where test reports are stored
+test_report_dir = "parallel-backend-tests/core/build/test-results"
+merged_report = JUnitXml()
+
+# Loop through all XML files and merge them
+for root, dirs, files in os.walk(test_report_dir):
+    for file in files:
+        if file.endswith(".xml"):
+            report_path = os.path.join(root, file)
+            xml = JUnitXml.fromfile(report_path)
+            merged_report += xml
+
+# Save the merged report
+merged_report.write("unified-test-results/unified-report.xml")
